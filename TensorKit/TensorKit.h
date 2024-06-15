0e473f1bf20b5d36b5590f40210e4ac66b0df108
Michael Tamburello <michaeljtamburello@gmail.com>
2024-06-15 16:07:09 -0500

    Initial Commit

---
+++ TensorKit/TensorKit.h
@@ -0,0 +1,18 @@
+//
+//  TensorKit.h
+//  TensorKit
+//
+//  Created by Michael Tamburello on 6/15/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TensorKit.
+FOUNDATION_EXPORT double TensorKitVersionNumber;
+
+//! Project version string for TensorKit.
+FOUNDATION_EXPORT const unsigned char TensorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TensorKit/PublicHeader.h>
+
+
